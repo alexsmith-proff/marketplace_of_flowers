ef0bc6767dbdc31454745000486823340713a980
alexsmith_proff <alexsmith_proff@mail.ru>
2023-01-10 17:54:32 +0300

    corrected

--- backend/src/imgelement/imgelement.controller.ts
+++ backend/src/imgelement/imgelement.controller.ts
@@ -7,21 +7,9 @@ import { ImgElementService } from './imgelement.service';
 export class ImgElementController {
   constructor(private readonly imgElementService: ImgElementService) { }
 
-  // @Get(':slug')
-  //   findBySlug(@Param() params){
-  //       console.log('nkkkkkmnmnmnmnmnmnmnmn');
-  //       return 'this.menuService.findBySlug(params.slug)'
-  //   }
-
-//   @Post('create')
-//   findBySlug(@Param() params){
-  //     return 'jhkjhj'
-  // }
-  
   @Post('create')
   @UseInterceptors(FileInterceptor('file'))
   create(@UploadedFile() file: Express.Multer.File, @Body() CreateImgElementInput: CreateImgElementInput) {    
-        console.log('nmnmnmnmnmrrrrrrrrrrrrrrr');
     return this.imgElementService.create(file, CreateImgElementInput)
   }
 
