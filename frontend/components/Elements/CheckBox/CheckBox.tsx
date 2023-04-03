2136484e1fe9bbdddc05d48ca9cb00faef1f5e65
alexsmith-proff <alexsmith_proff@mail.ru>
2023-04-03 16:29:13 +0300

    frontend: corrected FilterCheckBox, added expand/collapse window

--- frontend/components/Elements/CheckBox/CheckBox.tsx
+++ frontend/components/Elements/CheckBox/CheckBox.tsx
@@ -5,13 +5,14 @@ import s from './CheckBox.module.scss'
 interface CheckBoxProps { 
     id: number
     name: string
+    checked: boolean
     onChangeCheckBox: () => void
 }
 
-const CheckBox: FC <CheckBoxProps> = ({ id, name, onChangeCheckBox }) => {
+const CheckBox: FC <CheckBoxProps> = ({ id, name, checked, onChangeCheckBox }) => {
     return (
         <li className={s.checkBox}>
-            <input className={s.box} type="checkbox" id={String(id)} onChange={onChangeCheckBox} />
+            <input className={s.box} type="checkbox" id={String(id)} checked={checked} onChange={onChangeCheckBox} />
             <label htmlFor={String(id)}>{name}</label>
         </li>
     )
--- frontend/components/Filter/FilterCheckBox/FilterCheckBox.tsx
+++ frontend/components/Filter/FilterCheckBox/FilterCheckBox.tsx
@@ -1,8 +1,11 @@
+import Image from 'next/image'
 import { FC, useContext, useState, useEffect } from 'react'
+import { URL } from 'url'
 import FilterContext from '../../../context/filter-context'
 import { IFilterElement, IFilterValue } from '../../../interfaces/filter.interface'
 import { getFilterElementFromFilterBySlug } from '../../../services/core/parse'
 import CheckBox from '../../Elements/CheckBox/CheckBox'
+import FilterSeparateLine from '../FilterSeparateLine/FilterSeparateLine'
 import FilterTitle from '../FilterTitle/FilterTitle'
 
 import s from './FilterCheckBox.module.scss'
@@ -16,6 +19,7 @@ const FilterCheckBox: FC<FilterCheckBoxProps> = ({ filterName, filterSlug }) => 
     const value = useContext(FilterContext)
 
     const [filterElement, setFilterElement] = useState<IFilterElement>()
+    const [isExpand, setIsExpand] = useState<boolean>(false)
 
     useEffect(() => {
         const element = getFilterElementFromFilterBySlug(value.filter, filterSlug)
@@ -31,18 +35,43 @@ const FilterCheckBox: FC<FilterCheckBoxProps> = ({ filterName, filterSlug }) => 
         })
     }
 
+    const HandleExpand = () => {
+        setIsExpand(!isExpand)
+    }
+
     return (
         <div className={s.block}>
             <FilterTitle title={filterName} />
             <ul className={s.list}>
                 {
-                    filterElement?.values?.map((item) => (
-                    <div className={s.checkBox}>
-                        <CheckBox id={item.id} name={item.name} key={item.id} onChangeCheckBox={() => handleChangeCheckBoxFilterElement(item)} />
-                    </div>
+                    filterElement?.values?.map((item, index) => (
+                        <>
+                            {
+                                isExpand && (
+                                    <div className={s.checkBox}>
+                                        <CheckBox id={item.id} name={item.name} checked={item.value === '1' ? true : false} key={item.id} onChangeCheckBox={() => handleChangeCheckBoxFilterElement(item)} />
+                                    </div>
+                                )
+                            }
+                            {
+                                !isExpand && index <= 5 && (
+                                    <div className={s.checkBox}>
+                                        <CheckBox id={item.id} name={item.name} checked={item.value === '1' ? true : false} key={item.id} onChangeCheckBox={() => handleChangeCheckBoxFilterElement(item)} />
+                                    </div>
+                                )
+                            }
+                        </>
                     ))
                 }
             </ul>
+            {
+                filterElement?.values.length > 6 &&
+                <div className={!isExpand ? s.expand + ' ' + s.collapse : s.expand} onClick={HandleExpand} >
+                    <Image src='/img/filter-expand.png' width={20} height={20} alt='expand-ico' />
+                </div>
+            }
+
+            <FilterSeparateLine />
         </div>
     )
 }
