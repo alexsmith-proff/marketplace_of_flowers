0bda2cdfa06b70707e124c9445d242b6ea6df85a
alexsmith-proff <alexsmith_proff@mail.ru>
2023-05-05 17:47:37 +0300

    frontend: created ProductCardImageSlider

--- frontend/components/ProductCard/ProductCard.tsx
+++ frontend/components/ProductCard/ProductCard.tsx
@@ -2,16 +2,31 @@ import { FC } from 'react'
 import { IProduct } from '../../interfaces/products.interface'
 
 import s from './ProductCard.module.scss'
+import ProductCardDescription from './ProductCardDescription/ProductCardDescription'
+import ProductCardImageSlider from './ProductCardImageSlider/ProductCardImageSlider'
+import ProductCardContent from './ProductCardContent/ProductCardContent'
+import ProductCardDelivery from './ProductCardDelivery/ProductCardDelivery'
 
 type TProductCard = {
     product: IProduct
 }
 
-const ProductCard: FC<TProductCard> = ({ }) => {
+const ProductCard: FC<TProductCard> = ({ product }) => {
     return (
         <div className={s.product}>
             <div className='container'>
-                <div>ProductCard</div>
+                <div className={s.wrap}>
+                    <div>
+                        <div className={s.top}>
+                            <ProductCardImageSlider mainImage={product.main_image} images={product.filenames_images} />
+                            <ProductCardContent />
+                        </div>
+                        <div className={s.description}>
+                            <ProductCardDescription />
+                        </div>
+                    </div>
+                    <ProductCardDelivery />
+                </div>
             </div>
         </div>
     )
--- frontend/components/ProductCard/ProductCardImageSlider/ProductCardImageSlider.tsx
+++ frontend/components/ProductCard/ProductCardImageSlider/ProductCardImageSlider.tsx
@@ -1,16 +1,34 @@
-import { FC } from 'react'
+import { FC, useState } from 'react'
 
 import s from './ProductCardImageSlider.module.scss'
+import Image from 'next/image'
 
 type TProductCardImageSlider = {
+    mainImage: string
+    images: string[]
 }
 
-const ProductCardImageSlider: FC<TProductCardImageSlider> = ({ }) => {
+const ProductCardImageSlider: FC<TProductCardImageSlider> = ({ mainImage, images }) => {
+    const [mainimage, setMainImage] = useState<string>(mainImage)
+
+    const handleClickThumbnail = (index) => {
+        setMainImage(images[index])
+    }
+
     return (
-        <div className={s.product}>
-            <div className='container'>
-                <div>ProductCardImageSlider</div>
+        <div className={s.slider}>
+            <div className={s.mainimage}>
+                <Image src={`${process.env.API_URI_DOCKER}/${mainimage}`} width={676} height={400} objectFit='cover' alt='product-main-img' />
             </div>
+            <ul className={s.thumbnails}>
+                {
+                    images.map((img, index) => (
+                        <div className={s.thumbnailImg} onClick={() => handleClickThumbnail(index)}>
+                            <Image className={s.thumbnailImg} src={`${process.env.API_URI_DOCKER}/${img}`} width={112} height={90} objectFit='fill' key={index} alt='product-thumbnail-img' />
+                        </div>
+                    ))
+                }
+            </ul>
         </div>
     )
 }
