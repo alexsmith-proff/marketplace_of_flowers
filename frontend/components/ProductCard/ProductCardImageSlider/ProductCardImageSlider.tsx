1f2d0d6d6a7d8779f927d6df63fb402df26bf488
alexsmith-proff <alexsmith_proff@mail.ru>
2023-06-19 15:48:00 +0300

    frontend: added zoom to the ProductCardImageSlider

--- frontend/components/ProductCard/ProductCardImageSlider/ProductCardImageSlider.tsx
+++ frontend/components/ProductCard/ProductCardImageSlider/ProductCardImageSlider.tsx
@@ -1,7 +1,11 @@
-import { FC, useState } from 'react'
+import { FC, useRef, useState } from 'react'
 
 import s from './ProductCardImageSlider.module.scss'
 import Image from 'next/image'
+// import Zoom from 'react-medium-image-zoom'
+// import { TransformWrapper } from "react-zoom-pan-pinch";
+// import ReactImageZoom from 'react-image-zoom';
+// import ImageZoom from "react-image-zooom";
 
 type TProductCardImageSlider = {
     mainImage: string
@@ -10,6 +14,25 @@ type TProductCardImageSlider = {
 
 const ProductCardImageSlider: FC<TProductCardImageSlider> = ({ mainImage, images }) => {
     const [mainimage, setMainImage] = useState<string>(mainImage)
+    const [xy, setXY] = useState({
+        x: 0,
+        y: 0,
+        transform: 1
+    })
+
+    const imgRef = useRef<HTMLImageElement>();
+
+    const handleMouseMoveImg = (e) => {
+        // console.log('X', e.pageX - imgRef.current.offsetLeft)
+        // console.log('Y', e.pageY - imgRef.current.offsetTop)
+        const x = -Math.round((e.pageX - imgRef.current.offsetLeft)*1.0) + (476*2.0-476)/2
+        const y = -Math.round((e.pageY - imgRef.current.offsetTop)*1.0) + (400*2.0-400)/2
+        setXY({ ...xy, x: x, y: y, transform: 2.0 })
+    }
+
+    const handleMouseLeaveImg = (e) => {
+        setXY({ ...xy, x: 0, y: 0, transform: 1 })
+    }
 
     const handleClickThumbnail = (index) => {
         setMainImage(images[index])
@@ -17,8 +40,36 @@ const ProductCardImageSlider: FC<TProductCardImageSlider> = ({ mainImage, images
 
     return (
         <div className={s.slider}>
-            <div className={s.mainimage}>
-                <Image src={`${process.env.API_URI_DOCKER}/${mainimage}`} width={476} height={400} objectFit='cover' alt='product-main-img' />
+            <div className={s.mainimage} style={{ width: '476px', overflow: 'hidden', height: '400px' }}
+                onMouseMove={handleMouseMoveImg}
+                onMouseLeave={handleMouseLeaveImg}
+                ref={imgRef}
+            >
+                {/* <Image src={`${process.env.API_URI_DOCKER}/${mainimage}`}
+                    width={476}
+                    height={400}
+                    objectFit='cover'
+                    alt='product-main-img'
+                    style={{
+                        transform: 'scale(1.5)',
+                        position: 'relative',
+                        // top: '50px !important',
+                    }} /> */}
+
+                <img
+                    src={`${process.env.API_URI}/${mainimage}`}
+                    alt="A image to apply the ImageZoom plugin"
+                    style={
+                        {
+                            left: `${xy.x}px`,
+                            top: `${xy.y}px`,
+                            transform: `scale(${xy.transform})`
+                        }
+                    }
+                />
+
+
+
             </div>
             <ul className={s.thumbnails}>
                 {
