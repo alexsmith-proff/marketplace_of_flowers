d30beffa561ea9737fc71fc6adfbaf9b0e2d69bf
alexsmith-proff <alexsmith_proff@mail.ru>
2023-06-22 13:40:19 +0300

    frontend: added CartContent, CartList, CartItem

---
+++ frontend/components/CartContent/CartContent.tsx
@@ -0,0 +1,12 @@
+import { FC } from "react";
+
+import s from './CartContent.module.scss'
+
+interface CartContentProps {}
+const CartContent: FC<CartContentProps> = ({  }) => {
+    return (
+        <div></div>
+    )
+}
+
+export default CartContent
