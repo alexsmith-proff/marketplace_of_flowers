42efe5e7b4394890b946720a36534722c3be72a1
alexsmith-proff <alexsmith_proff@mail.ru>
2023-06-22 13:51:16 +0300

    added CartTotal

--- frontend/components/CartContent/CartContent.tsx
+++ frontend/components/CartContent/CartContent.tsx
@@ -1,11 +1,21 @@
 import { FC } from "react";
 
 import s from './CartContent.module.scss'
+import CartList from "../CartList/CartList";
+import CartTotal from "../CartTotal/CartTotal";
 
 interface CartContentProps {}
 const CartContent: FC<CartContentProps> = ({  }) => {
     return (
-        <div></div>
+        <div>
+            <h1 className={s.title}>Корзина</h1>
+            <div className={s.wrap}>
+                <CartList />
+                <div>
+                    <CartTotal />
+                </div>
+            </div>
+        </div>
     )
 }
 
---
+++ frontend/components/CartTotal/CartTotal.tsx
@@ -0,0 +1,12 @@
+import { FC } from "react";
+
+import s from './CartTotal.module.scss'
+
+interface CartTotalProps {}
+const CartTotal: FC<CartTotalProps> = ({  }) => {
+    return (
+        <div></div>
+    )
+}
+
+export default CartTotal
