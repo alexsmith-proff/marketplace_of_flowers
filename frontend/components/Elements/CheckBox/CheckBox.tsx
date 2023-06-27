29010e896b783c67175c1930fd27ca78297cfc0d
alexsmith-proff <alexsmith_proff@mail.ru>
2023-06-27 13:39:41 +0300

    frontend: corrected styles CheckBox

--- frontend/components/Elements/CheckBox/CheckBox.tsx
+++ frontend/components/Elements/CheckBox/CheckBox.tsx
@@ -2,19 +2,23 @@ import { FC } from 'react'
 
 import s from './CheckBox.module.scss'
 
-interface CheckBoxProps { 
+interface CheckBoxProps {
     id: number
     name: string
     checked?: boolean
     onChangeCheckBox: () => void
 }
 
-const CheckBox: FC <CheckBoxProps> = ({ id, name, checked, onChangeCheckBox }) => {
+const CheckBox: FC<CheckBoxProps> = ({ id, name, checked, onChangeCheckBox }) => {
+    // const background: string = '000' 
+
     return (
-        <li className={s.checkBox}>
-            <input className={s.box} type="checkbox" id={String(id)} checked={checked} onChange={onChangeCheckBox} />
-            <label htmlFor={String(id)}>{name}</label>
-        </li>
+        <>
+            <li className={s.checkBox}>
+                <input className={s.box} type="checkbox" id={String(id)} checked={checked} onChange={onChangeCheckBox} />
+                <label htmlFor={String(id)}>{name}</label>
+            </li>
+        </>
     )
 }
 
