4821fe5e71bef64d361d2710c37be110ba4cdd99
alexsmith-proff <alexsmith_proff@mail.ru>
2023-09-12 14:32:24 +0300

    frontend: refactored MainLayout component Header

--- frontend/components/Header/Header.tsx
+++ frontend/components/Header/Header.tsx
@@ -1,4 +1,10 @@
 import React, { FC } from 'react';
+import HeaderLogo from './HeaderLogo/HeaderLogo';
+import HeaderFind from './HeaderFind/HeaderFind';
+import HeaderCall from './HeaderCall/HeaderCall';
+import HeaderText from './HeaderText/HeaderText';
+import HeaderFavoriteBtnWithCount from './HeaderFavoriteBtnWithCount/HeaderFavoriteBtnWithCount';
+import HeaderCart from './HeaderCart/HeaderCart';
 
 import s from './Header.module.scss'
 
@@ -11,17 +17,17 @@ interface HeaderProps {
     cartComponent: React.ReactNode
 }
 
-const Header: FC<HeaderProps> = ({ logoComponent, findComponent, callComponent, headerTextComponent, favoriteBtnComponent, cartComponent }) => {
+const Header: FC<HeaderProps> = () => {
     return (
         <div className={s.header}>
             <div className="container">
                 <div className={s.headerContainer}>
-                    {logoComponent}
-                    {headerTextComponent}
-                    {findComponent}
-                    {callComponent}
-                    {favoriteBtnComponent}
-                    {cartComponent}                   
+                    <HeaderLogo />
+                    <HeaderFind />
+                    <HeaderCall />
+                    <HeaderText text='Доставка цветов в Воронеже' />
+                    <HeaderFavoriteBtnWithCount />
+                    <HeaderCart />
                 </div>
             </div>
         </div>
