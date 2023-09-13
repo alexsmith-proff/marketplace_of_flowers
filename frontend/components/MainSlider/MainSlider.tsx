b90732a0608828849d4d6b0114e6a5d292e01fd9
alexsmith-proff <alexsmith_proff@mail.ru>
2023-09-13 19:02:38 +0300

    frontend: refactored MainSlider SOLID, cteated SmallSlider, SmallSliderItem

--- frontend/components/MainSlider/MainSlider.tsx
+++ frontend/components/MainSlider/MainSlider.tsx
@@ -1,14 +1,9 @@
 import React, { FC } from 'react';
-
-import 'slick-carousel/slick/slick.css';
-import 'slick-carousel/slick/slick-theme.css';
-import Slider, { Settings } from "react-slick";
+import BigSlider from './BigSlider/BigSlider';
+import SmallSlider from './SmallSlider/SmallSlider';
+import { ISection } from '../../interfaces/section.interface';
 
 import s from './MainSlider.module.scss'
-import { ISection } from '../../interfaces/section.interface';
-import { getFileNameInImgBlockFromElement, getTextInTextBlockFromElement } from '../../services/core/parse';
-import BigSliderItem from './BigSliderItem/BigSliderItem';
-import BigSlider from './BigSlider/BigSlider';
 
 interface MainSliderProps {
     bigSliderSection: ISection
@@ -16,70 +11,15 @@ interface MainSliderProps {
 }
 
 const MainSlider: FC<MainSliderProps> = ({ bigSliderSection, smallSliderSection }) => {
-    // const settingsBigSlider: Settings = {
-    //     // dots: true,
-    //     arrows: true,
-    //     fade: true,
-    //     infinite: true,
-    //     autoplay: true,
-    //     autoplaySpeed: 8000, // Время между кадрами 7 сек
-    //     speed: 5000, // Плавность перехода 3 сек
-    //     slidesToShow: 1,
-    //     slidesToScroll: 1
-    // };
-    const settingsSmallSlider: Settings = {
-        // dots: true,
-        arrows: true,
-        infinite: true,
-        autoplay: true,
-        autoplaySpeed: 8000, // Время между кадрами 7 сек
-        speed: 1000, // Плавность перехода 3 сек
-        slidesToShow: 1,
-        slidesToScroll: 1
-    };
-
-    // console.log('bigSliderSection', bigSliderSection);
-
     return (
-        <div className={s.mainSlider}>
+        <div className={s.sliders}>
             <div className="container">
-                <div className={s.mainSliderContainer}>
+                <div className={s.wrap}>
                     <BigSlider sliders={bigSliderSection.elements} />
-                    <div className="smallSlider">
-                        <div className={s.smallSlider}>
-                            <div className={s.smallSlider__title}>Букет недели</div>
-                            {
-                                smallSliderSection &&
-                                <Slider {...settingsSmallSlider}>
-                                    {
-                                        smallSliderSection && smallSliderSection.elements.map(item => (
-                                            <div className={s.smallSlider__item} key={item.id}>
-                                                <img src={process.env.API_URI + '/' + getFileNameInImgBlockFromElement(item, 'background')} alt="small-slider1" />
-                                                <div className={s.smallSlider__info}>
-                                                    <div className={s.smallSlider__infoTitle}>{getTextInTextBlockFromElement(item, 'nazvanie')}</div>
-                                                    <div className={s.smallSlider__infoSubTitle}>{getTextInTextBlockFromElement(item, 'opisanie')}</div>
-                                                    <div className={s.smallSlider__infoChart}>
-                                                        <div className={s.smallSlider__infoPriceWrap}>
-                                                            <div className={s.smallSlider__infoPrice}>{getTextInTextBlockFromElement(item, 'cena') + ' ₽'}</div>
-                                                            <div className={s.smallSlider__infoCrossPrice}>{getTextInTextBlockFromElement(item, 'staraya-cena') + ' ₽'}</div>
-                                                        </div>
-                                                        <div className={s.smallSlider__infoBtn}>
-                                                            <img src="img/cart-ico.png" alt="cart-ico" />
-                                                            <div className={s.smallSlider__infoBtnText}>В корзину</div>
-                                                        </div>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                        ))
-                                    }
-                                </Slider>
-                            }
-                        </div>
-                    </div>
+                    <SmallSlider sliders={smallSliderSection.elements} />
                 </div>
             </div >
         </div >
-
     );
 };
 
