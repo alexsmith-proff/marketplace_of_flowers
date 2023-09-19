0e3f110e86b5f377fda8197883034270001f18fd
alexsmith-proff <alexsmith_proff@mail.ru>
2023-09-19 18:20:22 +0300

    frontend: test BigSliderItem

--- frontend/modules/MainSlider/components/BigSliderItem/BigSliderItem.test.tsx
+++ frontend/modules/MainSlider/components/BigSliderItem/BigSliderItem.test.tsx
@@ -1,1 +1,46 @@
-test('')
+import { render, screen } from "@testing-library/react"
+import '@testing-library/jest-dom'
+import BigSliderItem from "./BigSliderItem"
+import { IElement } from "../../../../interfaces/section.interface"
+
+const data: IElement = {
+    id: 1,
+    name: 'data',
+    slug: 'data',
+    img_elements: [
+        {
+            id: 1,
+            name: 'изображение',
+            slug: 'background',
+            filename: '1.png',
+
+        }
+    ],
+    text_elements: [
+        {
+            id: 1,
+            name: 'текст в верху',
+            slug: 'tekst-vverkhu',
+            text: 'верхний текст'
+        },
+        {
+            id: 2,
+            name: 'текст в середине',
+            slug: 'tekst-v-seredine',
+            text: 'средний текст'
+        },
+        {
+            id: 3,
+            name: 'текст промокод',
+            slug: 'promokod-tekst',
+            text: 'промокод'
+        }
+    ],
+    product_ref: null
+}
+test('BigSliderItem текст вверху', () => {
+    process.env.API_URI_DOCKER = 'http://localhost'
+    render(<BigSliderItem slider={data} />)
+    const  el = screen.getByText(/верхний текст/i)
+    expect(el).toBeInTheDocument()
+})
