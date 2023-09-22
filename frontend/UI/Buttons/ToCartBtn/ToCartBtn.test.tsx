dc268a188cd66807866827b6424b0ae3fd837c11
alexsmith-proff <alexsmith_proff@mail.ru>
2023-09-22 13:40:50 +0300

    test: added ToCartBtn

---
+++ frontend/UI/Buttons/ToCartBtn/ToCartBtn.test.tsx
@@ -0,0 +1,23 @@
+import '@testing-library/jest-dom'
+import { render, screen } from "@testing-library/react"
+import ToCartBtn from "./ToCartBtn"
+
+describe('ToCartBtn', () => {
+    it('ToCartBtn render', ()=>{
+        render(<ToCartBtn />)
+        expect(screen.getByText(/в корзину/i)).toBeInTheDocument()
+    })
+    it('ToCartBtn isBuyProduct', ()=>{
+        render(<ToCartBtn isBuyProduct={true}/>)
+        expect(screen.getByText(/в корзине/i)).toBeInTheDocument()
+    })
+    it('ToCartBtn содержит textAfterClick', ()=>{
+        render(<ToCartBtn isBuyProduct={true} textAfterClick='текст' />)
+        expect(screen.getByText(/текст/i)).toBeInTheDocument()
+    })
+    it('ToCartBtn темный', ()=>{
+        const { container } = render(<ToCartBtn dark={true} />)
+        const boxes = container.getElementsByClassName('dark')
+        expect(boxes.length).toBe(1)
+    })
+})
